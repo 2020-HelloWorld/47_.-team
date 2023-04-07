bb86a8f2b2be6dcca44b3058052e45f5ab0b3085
hitesh9929 <hiteshabishnoi@gmail.com>
2023-04-07 15:32:58 +0530

    added home page design

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="welcome-page">
+      <h1>Welcome!</h1>
+      <Link to='/login'>
+        <button className="animate-button">Get Started</button>
+      </Link>
+    </div>
+  );
+
 }
 
 export default Home
