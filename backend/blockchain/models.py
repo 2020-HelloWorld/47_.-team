b17a97c330f5309fe502f6506a5461f31237d06c
Ghanashyam Bhat <ghanashyambhat6@gmail.com>
2023-07-03 19:10:02 +0530

    models for blockchain

--- backend/blockchain/models.py
+++ backend/blockchain/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class block(models.Model):
+    index = models.IntegerField(primary_key=True)
+    proof = models.IntegerField()
+    prevHash = models.CharField(max_length=50)
+    data = models.CharField(max_length=500)
+    timestamp = models.DateTimeField()
