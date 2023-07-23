e3441c887267e478180ae58eb1d100a53ad3ed97
Ghanashyam Bhat <ghanashyambhat6@gmail.com>
2023-07-23 23:46:41 +0530

    projects

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.test)
+    path('add/',views.addnewProject),
+    path('student/',views.addNewStudent),
+    path('list/',views.listProjects),
+    path('details/',views.projectDetails),
+    path('approval/list/',views.approvalList),
+    path('approval/',views.approve),
 ]
